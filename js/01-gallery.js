cc80154435ec5d099c5ff5526cfb13ed489d277f
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-03 22:58:08 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const listEl = document.querySelector(".gallery");
+console.log(listEl);
+
+const markup = galleryItems.map((img) => {
+  `<li class="gallery__item">
+        <a class="gallery__link" href="${img.original}">
+            <img
+                class="gallery__image"
+                src="small-image.jpg"
+                data-source="${img.original}"
+                alt="${img.description}"
+            />
+        </a>
+    </li>`;
+});
+
+console.log(markup);
 
 console.log(galleryItems);
