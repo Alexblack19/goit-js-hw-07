966de539c707531227e2653276e285e65b905585
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-06 15:11:16 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,6 +2,32 @@ import { galleryItems } from './gallery-items.js';
 
 // Change code below this line
 
+// const galleryListEl = document.querySelector('.gallery');
+// const galleryMarkup = createImgGalleryMarkup(galleryItems);
+// galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// function createImgGalleryMarkup(galleryImjArr) {
+//     return galleryImjArr
+//         .map(({ original, preview, description }) => {
+//             return `<li class="gallery__item">
+//                         <a class="gallery__link" href="${original}">
+//                             <img
+//                                 class="gallery__image"
+//                                 src="${preview}"
+//                                 alt="${description}"
+//                             />
+//                         </a>
+//                     </li>`;
+//         })
+//         .join('');
+// }
+
+// const galleryLightbox = new SimpleLightbox('.gallery__item .gallery__link', {
+//     captionsData: 'alt',
+//     captionDelay: 250,
+//     enableKeyboard: true,
+// });
+
 const galleryListEl = document.querySelector('.gallery');
 const galleryMarkup = createImgGalleryMarkup(galleryItems);
 galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
@@ -22,11 +48,12 @@ function createImgGalleryMarkup(galleryImjArr) {
         .join('');
 }
 
-const galleryLightbox = new SimpleLightbox('.gallery__item .gallery__link', {
+let galleryLightbox = new SimpleLightbox('.gallery__item .gallery__link', {
     captionsData: 'alt',
     captionDelay: 250,
     enableKeyboard: true,
 });
+
 //================================================
 // Закриття lightbox натисканням на клавішу Space
 //================================================
