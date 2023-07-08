707f47340f4cfe29e448c50e0d96fefe229662f0
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-08 15:03:53 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,10 +2,59 @@ import { galleryItems } from './gallery-items.js';
 
 // Change code below this line
 
+// const galleryListEl = document.querySelector('.gallery');
+// const galleryMarkup = createImgGalleryMarkup(galleryItems);
+
+// function createImgGalleryMarkup(galleryImjArr) {
+//     return galleryImjArr
+//         .map(({ original, preview, description }) => {
+//             return `<li class="gallery__item">
+//                         <a class="gallery__link" href="${original}">
+//                             <img
+//                                 class="gallery__image"
+//                                 src="${preview}"
+//                                 data-source="${original}"
+//                                 alt="${description}"
+//                             />
+//                         </a>
+//                     </li>`;
+//         })
+//         .join('');
+// }
+// galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// galleryListEl.addEventListener('click', onImgClick);
+// function onImgClick(event) {
+//     event.preventDefault();
+//     if (!event.target.classList.contains('gallery__image')) {
+//         return;
+//     }
+//     modalOpen(event);
+// }
+
+// function modalOpen(event) {
+//     const originalImg = `<img src="${event.target.dataset.source}"/>`;
+//     const modal = basicLightbox.create(`${originalImg}`);
+//     modal.show();
+//     modalClose(modal);
+// }
+
+// function modalClose(modal) {
+//     document.addEventListener('keydown', onEscKeydown);
+//     function onEscKeydown(event) {
+//         console.log(event);
+//         if (event.code !== 'Escape') {
+//             return;
+//         }
+//         modal.close();
+//         document.removeEventListener('keydown', onEscKeydown);
+//     }
+// }
+//========================================================================
+//========================================================================
+
 const galleryListEl = document.querySelector('.gallery');
 const galleryMarkup = createImgGalleryMarkup(galleryItems);
-galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
-galleryListEl.addEventListener('click', onImgClick);
 
 function createImgGalleryMarkup(galleryImjArr) {
     return galleryImjArr
@@ -23,29 +72,38 @@ function createImgGalleryMarkup(galleryImjArr) {
         })
         .join('');
 }
+galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
 
+galleryListEl.addEventListener('click', onImgClick);
 function onImgClick(event) {
+    console.log(event.target);
     event.preventDefault();
     if (!event.target.classList.contains('gallery__image')) {
         return;
     }
-    modalOpen(event);
+    return event;
 }
 
-function modalOpen(event) {
-    const originalImg = `<img src="${event.target.dataset.source}"/>`;
-    const modal = basicLightbox.create(`${originalImg}`);
-    modal.show();
-    modalClose(modal);
-}
+const event = onImgClick();
+const originalImg = `<img src="${event.target.dataset.source}"/>`;
+console.log(galleryListEl.addEventListener('click', onImgClick));
+const modal = basicLightbox.create(`${originalImg}`);
 
-function modalClose(modal) {
-    document.addEventListener('keydown', onEscKeydown);
-    function onEscKeydown(event) {
-        if (event.code !== 'Escape') {
-            return;
-        }
-        modal.close();
-        document.removeEventListener('keydown', onEscKeydown);
-    }
-}
+// function modalOpen(event) {
+//     const originalImg = `<img src="${event.target.dataset.source}"/>`;
+//     const modal = basicLightbox.create(`${originalImg}`);
+//     modal.show();
+//     modalClose(modal);
+// }
+
+// function modalClose(modal) {
+//     document.addEventListener('keydown', onEscKeydown);
+//     function onEscKeydown(event) {
+//         console.log(event);
+//         if (event.code !== 'Escape') {
+//             return;
+//         }
+//         modal.close();
+//         document.removeEventListener('keydown', onEscKeydown);
+//     }
+// }
