025200fc1b559650979f951ee5983c34da53533a
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-08 15:25:10 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,8 @@
 import { galleryItems } from './gallery-items.js';
-
 // Change code below this line
-
+//*============
+// * Варіант 1
+//*============
 // const galleryListEl = document.querySelector('.gallery');
 // const galleryMarkup = createImgGalleryMarkup(galleryItems);
 
@@ -52,7 +53,9 @@ import { galleryItems } from './gallery-items.js';
 // }
 //========================================================================
 //========================================================================
-
+//*============
+// * Варіант 2
+//*============
 const galleryListEl = document.querySelector('.gallery');
 const galleryMarkup = createImgGalleryMarkup(galleryItems);
 
@@ -76,34 +79,25 @@ galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
 
 galleryListEl.addEventListener('click', onImgClick);
 function onImgClick(event) {
-    console.log(event.target);
     event.preventDefault();
     if (!event.target.classList.contains('gallery__image')) {
         return;
     }
-    return event;
+    const originalImg = `<img src="${event.target.dataset.source}"/>`;
+    const modal = basicLightbox.create(`${originalImg}`, {
+        onShow: modal => {
+            document.addEventListener('keydown', onEscKeydownCloseModal);
+        },
+        onClose: modal => {
+            document.removeEventListener('keydown', onEscKeydownCloseModal);
+        },
+    });
+    modal.show();
+
+    function onEscKeydownCloseModal(event) {        
+        if (event.code !== 'Escape') {
+            return;
+        }
+        modal.close();
+    }
 }
-
-const event = onImgClick();
-const originalImg = `<img src="${event.target.dataset.source}"/>`;
-console.log(galleryListEl.addEventListener('click', onImgClick));
-const modal = basicLightbox.create(`${originalImg}`);
-
-// function modalOpen(event) {
-//     const originalImg = `<img src="${event.target.dataset.source}"/>`;
-//     const modal = basicLightbox.create(`${originalImg}`);
-//     modal.show();
-//     modalClose(modal);
-// }
-
-// function modalClose(modal) {
-//     document.addEventListener('keydown', onEscKeydown);
-//     function onEscKeydown(event) {
-//         console.log(event);
-//         if (event.code !== 'Escape') {
-//             return;
-//         }
-//         modal.close();
-//         document.removeEventListener('keydown', onEscKeydown);
-//     }
-// }
